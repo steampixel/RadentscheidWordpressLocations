5ab5782cc4ee8a9cbcab2dff357df4a6e99c63df
SteamPixel <info@steampixel.de>
2019-11-04 17:03:17 +0100

    Changed wording

--- views/backened_location_activist.php
+++ views/backened_location_activist.php
@@ -14,9 +14,9 @@
       This plugin is programmed to the best of my knowledge and belief. But I can not rule out mistakes.
       In addition, Wordpress and its plugin ecosystem are known for its numerous security holes in history.
       You should therefore delete the data of the activists when you no longer need them.
-      The worst case would be if unprivileged have access to the data of activists.
+      The worst case would be if unprivileged have access to this data.
     </p>
-    <input type="submit" value="Permanently destroy this data" name="sp_remove_activist_data">
+    <input type="submit" value="Permanently destroy this contact data" name="sp_remove_activist_data">
   </label>
 
   <?PHP
