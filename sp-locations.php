b793ca77a7c4bddcf1d27e65ec08af8c460d77b8
Chris <chris@bueropersch.de>
2020-11-20 10:03:36 +0100

    Moved to version 1.10.0

--- sp-locations.php
+++ sp-locations.php
@@ -2,53 +2,17 @@
 /**
 * Plugin Name: Verkehrswende Locations
 * Description: Dieses Plugin hilft uns verschiedene Problemstellen für den Rad- und Fußverkehr zu tracken.
-* Version: 1.9.0
+* Version: 1.10.0
 * Author: Christoph Stitz
 * Author URI: https://steampixel.de
 **/
 
-define('SP_LOCATIONS_VERSION', '1.9.0');
+define('SP_LOCATIONS_VERSION', '1.10.0');
 
 // Debug
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
-
-// Include core classes
-include('classes/Thumbnail.php');
-include('classes/View.php');
-include('classes/Migrator.php');
-include('classes/Seeder.php');
-
-// Include API endpoints
-include('endpoints/sp_location_add.php');
-include('endpoints/sp_location_get.php');
-include('endpoints/sp_location_print.php');
-
-// Include shortcodes
-include('shortcodes/steampixel_marker_map.php');
-include('shortcodes/steampixel_marker_count.php');
-include('shortcodes/steampixel_marker_form.php');
-
-// Include custom post types
-include('custom_post_types/location.php');
-include('custom_post_types/marker.php');
-// include('custom_post_types/image.php');
-
-/*
-  Enable file upload in edit form
-*/
-add_action('post_edit_form_tag', function () {
-  echo ' enctype="multipart/form-data"';
-});
-
-/*
-  Run migrations and seeds
-*/
-add_action( 'init', function () {
-  Sp\Migrator::migrate();
-  Sp\Seeder::seed();
-});
 
 /*
   Function to get the upload url
@@ -72,6 +36,64 @@ function spTrimText($s, $max_length = 340) {
   }
   return $s;
 }
+
+/*
+  Function to grab an image
+  https://stackoverflow.com/questions/6476212/save-image-from-url-with-curl-php
+*/
+function grab_image($url,$saveto){
+  $ch = curl_init ($url);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+  $raw=curl_exec($ch);
+  curl_close ($ch);
+  if(file_exists($saveto)){
+    unlink($saveto);
+  }
+  $fp = fopen($saveto,'x');
+  fwrite($fp, $raw);
+  fclose($fp);
+}
+
+// Include core classes
+include('classes/Thumbnail.php');
+include('classes/View.php');
+include('classes/Migrator.php');
+include('classes/Seeder.php');
+
+// Include API endpoints
+include('endpoints/sp_location_add.php');
+include('endpoints/sp_location_get.php');
+include('endpoints/sp_location_print.php');
+include('endpoints/sp_location_import.php');
+include('endpoints/sp_geojson_get.php');
+
+// Include shortcodes
+include('shortcodes/steampixel_marker_map.php');
+include('shortcodes/steampixel_marker_count.php');
+include('shortcodes/steampixel_marker_form.php');
+
+// Include custom post types
+include('custom_post_types/location.php');
+include('custom_post_types/marker.php');
+include('custom_post_types/geojson.php');
+// include('custom_post_types/image.php');
+
+/*
+  Enable file upload in edit form
+*/
+add_action('post_edit_form_tag', function () {
+  echo ' enctype="multipart/form-data"';
+});
+
+/*
+  Run migrations and seeds
+*/
+add_action( 'init', function () {
+  Sp\Migrator::migrate();
+  Sp\Seeder::seed();
+});
 
 /*
   Register plugin options
